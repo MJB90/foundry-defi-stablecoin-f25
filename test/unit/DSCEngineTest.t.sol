99ae22b61cd98a66e70a1b18dcf529da0eb4283a
mjb90 <manasborah90@gmail.com>
2025-05-18 17:06:06 +0800

    deposit collateral test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DSCEngine public s_dscEngine;
@@ -13,11 +14,15 @@ contract DSCEngineTest is Test {
     HelperConfig public s_helperConfig;
     address wethUsdPriceFeed;
     address weth;
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
         DeployDSC deployer = new DeployDSC();
         (s_dscEngine, s_dsc, s_helperConfig) = deployer.run();
         (wethUsdPriceFeed,, weth,,,) = s_helperConfig.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     ////////////////////////////////////////////////////////////////////
@@ -28,5 +33,15 @@ contract DSCEngineTest is Test {
         uint256 expectedValue = 30000e18; // 15 * 2000
         uint256 actualValue = s_dscEngine.getUsdValue(weth, amount);
         assertEq(actualValue, expectedValue, "getUsdValue failed");
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    //                            Deposit Collateral Tests
+    ////////////////////////////////////////////////////////////////////
+    function testRevertIfCollateralIsZero() public {
+        vm.prank(USER);
+        ERC20Mock(weth).approve(address(s_dscEngine), AMOUNT_COLLATERAL);
+        vm.expectRevert(abi.encodeWithSelector(DSCEngine.DSCEngine_NeedsMoreThanZero.selector));
+        s_dscEngine.depositCollateral(weth, 0);
     }
 }
