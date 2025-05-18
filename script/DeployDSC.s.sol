88f35f66f579ff542b064fca2f5bad4b3a66a13c
mjb90 <manasborah90@gmail.com>
2025-05-18 15:34:46 +0800

    Deploy Script

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+import {Script} from "forge-std/Script.sol";
+
+pragma solidity ^0.8.18;
+
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DSCEngine, DecentralizedStableCoin) {
+        HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        vm.startBroadcast(deployerKey);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin(msg.sender);
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(dscEngine));
+        vm.stopBroadcast();
+        return (dscEngine, dsc);
+    }
+}
