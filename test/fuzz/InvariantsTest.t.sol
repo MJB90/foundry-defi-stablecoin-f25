6838ff0fc24af48d0ba54be189660e2cf82df712
mjb90 <manasborah90@gmail.com>
2025-05-21 16:47:12 +0800

    Invariant test fmt

--- test/fuzz/InvariantsTest.t.sol
+++ test/fuzz/InvariantsTest.t.sol
@@ -4,6 +4,7 @@
 //1. The total supply of DSC should always be less than the total values of the collateral
 //2. Getter view functions should never revert
 pragma solidity ^0.8.18;
+
 import {Test, console} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
@@ -11,6 +12,4 @@ import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
-contract InvariantsTest{
-
-}
+contract InvariantsTest {}
