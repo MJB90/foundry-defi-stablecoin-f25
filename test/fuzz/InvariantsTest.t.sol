cd048312135471a6da9ef7f845cb40c64d61bb3a
mjb90 <manasborah90@gmail.com>
2025-05-21 16:46:57 +0800

    Invariant tests

---
+++ test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+// Should contain the properties of the contracts that should always hold true
+
+//1. The total supply of DSC should always be less than the total values of the collateral
+//2. Getter view functions should never revert
+pragma solidity ^0.8.18;
+import {Test, console} from "forge-std/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract InvariantsTest{
+
+}
